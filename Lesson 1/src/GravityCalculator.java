d4dff402234885c823814d8eade442523c8b9bda
seanpatrickmay <may.se@northeastern.edu>
2025-07-27 16:49:07 +0200

    Uploading lesson 1 files

---
+++ Lesson 1/src/GravityCalculator.java
@@ -0,0 +1,13 @@
+public class GravityCalculator {
+  public static void main(String[] arguments) {
+    double gravity = -9.81; // Gravity, in m/s^2                              (a)
+    double initialVelocity = 0.0; // Intial velocity of the object            (v_i)
+    double fallingTime = 10.0; // Duration, in seconds, of the object's fall  (t)
+    double initialPosition = 0.0; // Initial position of the object           (x_i)
+    double finalPosition = 0.5 * gravity * fallingTime * fallingTime
+        + initialVelocity * fallingTime + initialPosition; // Final position of the object
+    
+    System.out.println("The object's position after " + fallingTime
+        + " seconds is " + finalPosition + " meters.");
+  }
+}
---
+++ Lesson 1/src/StringExamples.java
@@ -0,0 +1,12 @@
+public class StringExamples {
+  public static void main(String[] arguments) {
+    String seperator = "------";
+    String myString = "Hello!";
+    
+    System.out.println(myString);
+    System.out.println(seperator);
+    
+    myString = "Goodbye!";
+    System.out.println(myString);
+  }
+}
